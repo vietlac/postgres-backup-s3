952e9ef5bbfb87e6692daec9931bc5e64a9c1882
Le Duy Quang <quangbahoa@users.noreply.github.com>
2025-03-17 09:08:58 +0700

    Forked
    First commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+# exit if a command fails
+set -eo pipefail
+
+apk update
+apk add openssl aws-cli 
+apk add postgresql-client --repository=https://dl-cdn.alpinelinux.org/alpine/v3.19/main
+
+# cleanup
+rm -rf /var/cache/apk/*
